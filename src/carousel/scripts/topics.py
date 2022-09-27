5c79f3a20562e36cd8dd43c2764cde07436059b6
Gabriel Blomberg <gabrielblomberg0@gmail.com>
2022-09-27 06:55:25 +0000

    Add topics.py for different topics.     - Added Topics enumeration of Topic, that are dataclasses       containing a topic string and the message type.     - Added subscriber() and publisher() methods to Topic       to create subscribers and publishers easily.

---
+++ src/carousel/scripts/topics.py
@@ -0,0 +1,61 @@
+import rospy as ros
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Callable
+
+from std_msgs.msg import Float32, ColorRGBA
+from geometry_msgs.msg import Pose
+from sensor_msgs.msg import JointState
+
+@dataclass
+class Topic:
+    """A general topic used by the carousel."""
+    string : str = None
+    type = None
+
+    def subscriber(self, callback : Callable):
+        """Create and return a subscriber to this topic.
+
+        Args:
+            callback: The callback to call when receiving a message on this
+                      topic.
+
+        Returns:
+            The subscriber to this topic.
+        """
+        return ros.Subscriber(self.string, self.type, callback)
+
+    def publisher(self, *, queue_size = 10):
+        """Create and return a publisher to this topic.
+
+        Args:
+            queue_size: The size of the message queue.
+
+        Returns:
+            A publisher to this topic.
+        """
+        return ros.Publisher(self.string, self.type, queue_size = queue_size)
+
+class Topics(Enum):
+    """A collection of topics used for communication between nodes in the
+    carousel code."""
+
+    # Topic about the fiducial boxes, their identifiers, orientation and
+    # positions.
+    box = Topic('carousel_box', Pose)
+
+    # Topic about the maximum colour observed by the camera.
+    max_colour = Topic('carousel_colour', ColorRGBA)
+
+    # Topic about the real life end effector configuration.
+    effector = Topic('carousel_effector', Pose)
+
+    # Topic about the desired configuration of the end effector.
+    effector_desired = Topic('carousel_effector_desired', Pose)
+
+    # Topic about the current gripper width.
+    gripper = Topic('carousel_gripper', Float32)
+
+    # Topic about the desired joint states provided by the dynamixel library.
+    desired_joint_states = Topic('desired_joint_states', JointState)
